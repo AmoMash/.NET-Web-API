eb6b2a30d9892e2901e9e082b73925dd96cdfd9c
Amogelang  Mashamaite <143009471+AmoMash@users.noreply.github.com>
2024-08-09 12:03:23 +0200

    Add files via upload

---
+++ AuthController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TelemetryAPI_3420.Models;
+
+namespace TelemetryAPI_3420.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public AuthController(JwtSettings jwtSettings)
+        {
+            _jwtSettings = jwtSettings;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel login)
+        {
+            // Validate the user credentials (this is just an example, you'd likely query your DB here)
+            if (login.Username == "user" && login.Password == "password")
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.UTF8.GetBytes(_jwtSettings.Key);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                    new Claim(ClaimTypes.Name, login.Username)
+                    }),
+                    Expires = DateTime.UtcNow.AddHours(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                    Audience = _jwtSettings.Audience,
+                    Issuer = _jwtSettings.Issuer
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return Ok(new { token = tokenHandler.WriteToken(token) });
+            }
+
+            return Unauthorized();
+        }
+    }
+}
+public class LoginModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
