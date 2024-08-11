5e839701c4f3ee139c3b87098985ec9b574dcb31
Amogelang  Mashamaite <143009471+AmoMash@users.noreply.github.com>
2024-08-11 23:01:19 +0200

    Delete JwtSettings.cs

--- JwtSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TelemetryAPI_3420.Models
-{
-    public class JwtSettings
-    {
-        public string Key { get; set; }
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
-    }
-}
