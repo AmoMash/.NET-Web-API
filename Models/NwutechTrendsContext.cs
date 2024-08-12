9f77259f93eb859f80b440b6b41244ef8b306dee
AmoMash <143009471+AmoMash@users.noreply.github.com>
2024-08-12 04:23:20 +0200

    Add project files.

---
+++ Models/NwutechTrendsContext.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TelemetryAPI_3420.Models;
+
+public partial class NwutechTrendsContext : DbContext
+{
+    public NwutechTrendsContext()
+    {
+    }
+
+    public NwutechTrendsContext(DbContextOptions<NwutechTrendsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    public virtual DbSet<JobTelemetry> JobTelemetries { get; set; }
+
+    public virtual DbSet<Process> Processes { get; set; }
+
+    public virtual DbSet<Project> Projects { get; set; }
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Client", "Configs");
+
+            entity.Property(e => e.ClientId).HasColumnName("ClientID");
+            entity.Property(e => e.DateOnboarded).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<JobTelemetry>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.ToTable("JobTelemetry", "Tellemetry");
+
+            entity.Property(e => e.AdditionalInfo).IsUnicode(false);
+            entity.Property(e => e.BusinessFunction).IsUnicode(false);
+            entity.Property(e => e.EntryDate).HasColumnType("datetime");
+            entity.Property(e => e.Geography).IsUnicode(false);
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.JobId)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("JobID");
+            entity.Property(e => e.ProccesId)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("ProccesID");
+            entity.Property(e => e.QueueId)
+                .IsUnicode(false)
+                .HasColumnName("QueueID");
+            entity.Property(e => e.StepDescription).IsUnicode(false);
+            entity.Property(e => e.UniqueReference).IsUnicode(false);
+            entity.Property(e => e.UniqueReferenceType).IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Process>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Process", "Configs");
+
+            entity.Property(e => e.DateSubmitted).HasColumnType("datetime");
+            entity.Property(e => e.DefaultBusinessFunction)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.DefaultGeography)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Platform)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.ProcessConfigUrl)
+                .IsUnicode(false)
+                .HasColumnName("ProcessConfigURL");
+            entity.Property(e => e.ProcessId).HasColumnName("ProcessID");
+            entity.Property(e => e.ProcessName).IsUnicode(false);
+            entity.Property(e => e.ProcessType).IsUnicode(false);
+            entity.Property(e => e.ProjectId).HasColumnName("ProjectID");
+            entity.Property(e => e.ReportUrl)
+                .IsUnicode(false)
+                .HasColumnName("ReportURL");
+            entity.Property(e => e.Submitter).IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Project>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Projects", "Configs");
+
+            entity.Property(e => e.ClientId).HasColumnName("ClientID");
+            entity.Property(e => e.ProjectCreationDate).HasColumnType("datetime");
+            entity.Property(e => e.ProjectDescription)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.ProjectId).HasColumnName("ProjectID");
+            entity.Property(e => e.ProjectName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.ProjectStatus)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using TelemetryAPI_3420.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+/* Configure JWT Authentication
+var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtSettings>();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings.Issuer,
+        ValidAudience = jwtSettings.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key))
+    };
+});*/
+
+//DbContext configuration
+builder.Services.AddDbContext<NwutechTrendsContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
